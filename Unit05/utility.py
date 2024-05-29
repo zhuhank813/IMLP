7d8362aca84053051f45f04cb410190c01c9933a
zhuhank813 <zhuhank813@gmail.com>
2024-05-29 23:03:19 +0800

    作業5-1完成

---
+++ Unit05/utility.py
@@ -0,0 +1,109 @@
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+
+#####################################################
+import pandas as pd
+def testcase_report(dataset,classifier,Xtestset,X_test,ground_truth):
+    Xtestset=Xtestset.copy()
+    pred=classifier.predict(X_test)
+    error_index=np.where(pred!=ground_truth)[0]
+    print('accuracy:',1-len(error_index)/len(Xtestset))
+    print('error_index :',error_index)
+    #print('miss classification :',pred[error_index])
+    print('number of errors :%d/%d'%(len(error_index),len(X_test)))
+    data_index=Xtestset.iloc[error_index].index
+
+    print('error data index:',data_index)
+    Xtestset['target']=ground_truth
+    Xtestset['pred']=pred
+    only_mis=Xtestset[Xtestset['pred']!=Xtestset['target']]
+    return only_mis,Xtestset
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.3f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    
+#############################################
+from matplotlib.colors import ListedColormap
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], 
+                    y=X[y == cl, 1],
+                    alpha=0.6, 
+                    c=[cmap(idx)],
+                    edgecolor='black',
+                    marker=markers[idx], 
+                    label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+
+	
+
+	
